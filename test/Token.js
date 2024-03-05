9cb2c845d4089f76667c9b684ee6a1045dfa997c
IT HIRED GUNS INC <ithiredguns@gmail.com>
2024-03-05 18:34:24 -0500

    5. Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -7,13 +7,14 @@ const tokens= (n) => {
 
 describe('Token',()=>{  
     
-    let token, accounts,deployer;
+    let token, accounts, deployer,receiver;
 
-    beforeEach(async ()=>{
+    beforeEach( async ()=>{
         const Token = await ethers.getContractFactory('Token');        
         token = await Token.deploy('Dapp University','DAPP',1000000); 
          accounts = await ethers.getSigners();
          deployer = accounts[0];
+         receiver = accounts[1];
     });
     
     describe('Deployment', ()=> {
@@ -42,12 +43,55 @@ describe('Token',()=>{
 
         });
         it('assign total supply to sender',async ()=>{  
-            console.log(deployer);
+            //console.log(deployer);
             expect(await token.balanceOf(deployer.address)).to.equals(totalSupply);
             console.log();
 
         });
 
     });
+    describe('Sending Token',()=>{
+        let amount, transaction, result ;
+        
+        describe('Success', ()=>{
+            beforeEach(async()=>{
+                amount = tokens(100);
+                transaction = await token.connect(deployer).transfer(receiver.address,amount);
+                result = await transaction.wait();
+    
+            });
+    
+            it('Transfers token balances',async()=>{ 
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900));
+                expect(await token.balanceOf(receiver.address)).to.equal(amount);
+            });
+    
+            it('Emits a Transfer event', async() => {
+                const event = result.events[0];
+              // console.log(event);
+                expect(event.event).to.equal('Transfer');
+                const args = event.args;
+                expect(args.from).to.equal(deployer.address);
+                expect(args.to).to.equal(receiver.address);
+                expect(args.value).to.equal(amount);
+            });
+        });
+        
+        describe('Failure',()=> {
+            it('rejects insufficient balances', async()=> {
+                //Transfer more tokens than deployer has - 100 M
+              const invalidAmount =   tokens(100000000);
+              await expect(token.connect(deployer).transfer(receiver.address,invalidAmount)).to.be.reverted;
 
+            });
+            it('rejects invalid receipient', async()=> {
+                //Transfer more tokens than deployer has - 100 M
+              const invalidAmount =   tokens(100000000);
+              await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000',invalidAmount)).to.be.reverted;
+
+            });
+
+        });
+
+    });
 });
